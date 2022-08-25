d6cd6ed25307ae373d0279a621f39e56dab340ec
liorwi <liorwi@wix.com>
2022-08-25 17:06:52 +0300

    basic offline chat is working, need ui scroll box and then going on to socketIO

--- src/features/chat/Chat.js
+++ src/features/chat/Chat.js
@@ -8,61 +8,84 @@ import {
   Button,
   Avatar,
   Grid,
+  Container,
+  TextField,
 } from "@mui/material";
 
-import { selectMessages } from "./chatSlice";
+import { selectMessages, postMessage } from "./chatSlice";
 import styles from "./Chat.module.css";
-
-function createMessage(key, sender, msg) {
-  return (
-    <React.Fragment key={key}>
-      <Box sx={{ minWidth: 350 }}>
-        <Card sx={{ minHeight: 70 }} elevation={3} color="primary.main">
-          <CardContent>
-            <Grid container columns={{ xs: 4, md: 12 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={2}>
-                  <Avatar alt="UNKNOWN" src="/broken-image.jpg" />
-                </Grid>
-                <Grid item xs={8}>
-                  <Typography
-                    sx={{ fontSize: 14 }}
-                    color="primary.contrastText"
-                    align="left"
-                  >
-                    {sender}
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid item xs={2}></Grid>
-              <Typography variant="h5" component="div"></Typography>
-            </Grid>
-            <Grid item xs={2} sx={{marginLeft: 7, marginTop: 0 }}>
-              <Typography
-                variant="body2"
-                align="left"
-                color="text.secondary"
-              >
-                {msg}
-              </Typography>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Box>
-    </React.Fragment>
-  );
-}
 
 export function Chat() {
   const messages = useSelector(selectMessages);
   const dispatch = useDispatch();
 
+  function createMessage(key, sender, msg) {
+    return (
+      <React.Fragment key={key}>
+        <Box sx={{ minWidth: 350, maxHeight:300, marginTop: 3, overflow: 'auto' }}>
+          <Card sx={{ minHeight: 70 }} elevation={3} color="primary.main">
+            <CardContent>
+              <Grid container columns={{ xs: 4, md: 12 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={2}>
+                    <Avatar alt="UNKNOWN" src="/broken-image.jpg" />
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Typography
+                      sx={{ fontSize: 14 }}
+                      color="primary.contrastText"
+                      align="left"
+                    >
+                      {sender}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={2}></Grid>
+              </Grid>
+              <Grid item xs={2} sx={{ marginLeft: 7, marginTop: 0 }}>
+                <Typography variant="body2" align="left" color="text.secondary">
+                  {msg}
+                </Typography>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Box>
+      </React.Fragment>
+    );
+  }
+
+  function sendMessage(event) {
+    if (event.key === "Enter") {
+      const newMessage = {
+        msg: event.target.value,
+      };
+      dispatch(postMessage(newMessage));
+    }
+  }
+
   return (
     <div>
       <h1>Chat</h1>
-      {messages.map((message) =>
-        createMessage(message.key, message.sender, message.msg)
-      )}
+      <Container
+        sx={{
+          border: "1px solid black",
+          padding: 3,
+          paddingTop: 0,
+          marginBottom: 5,
+        }}
+      >
+        {messages.map((message) =>
+          createMessage(message.key, message.sender, message.msg)
+        )}
+      </Container>
+      <Container>
+        <TextField
+          id="msgInput"
+          label="Enter message"
+          onKeyDown={sendMessage}
+          variant="outlined"
+        />
+      </Container>
     </div>
   );
 }
